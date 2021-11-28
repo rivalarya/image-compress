acbdf1722b6ce65e307a80707259ea41e3a1e229
Rival <rifall.osd26@gmail.com>
2021-11-28 19:30:02 +0700

    tambah komponen url,install apexcharts untuk visualize persen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import ReactDOM from 'react-dom';
 import UseFile from './components/Main/useFile/main';
 import UseUrl from './components/Main/useUrl/main';
+
+function callUrl() {
+  return ReactDOM.render( <UseUrl /> , document.getElementsByTagName('main')[0])
+}
 
 function App() {
   return (
@@ -8,8 +13,8 @@ function App() {
       <div className="choice">
         <label>Compress using :</label>
         <div className="choice-item">
-          <button onClick={UseFile}>Upload file</button>
-          <button onClick={UseUrl}>Url</button>
+          <button onClick={UseFile}>Upload Image</button>
+          <button onClick={callUrl}>Url</button>
         </div>
       </div>
     </>
--- src/components/Main/useUrl/main.js
+++ src/components/Main/useUrl/main.js
@@ -1,12 +1,111 @@
 import ReactDOM from 'react-dom';
+import "./main.css";
+import {
+    clickButton,
+    details,
+    downloadCompressedImage,
+    handleErrorFromCompressImage
+} from './_utils';
+import ApexCharts from 'apexcharts';
+
+const compress = (e) => {
+
+    e.preventDefault();    
+    clickButton.start(e); //nonaktifkan button
+    
+    if (e.target[0].value.search('http') < 0) e.target.lastChild.style.display = 'block'; // jika user memaukan url yg tidak memilik value 'http'
+
+    const quality = 90 //default 92
+    
+    let compressImage = new Promise(function (myResolve, myReject) {
+        let req = new XMLHttpRequest();
+        req.open('GET', `http://api.resmush.it/ws.php?img=${e.target[0].value}&qlty=${quality}`);
+        req.onload = function () {
+            if (req.status === 200) {
+                myResolve(JSON.parse(req.response));
+            } else {
+                myReject("Failed. Try again later");
+            }
+        };
+        req.send();
+    });
+    compressImage.then(
+        function (value) {
+            let link = value.dest // yg sudah di kompress
+            
+            if (!value.hasOwnProperty('error')) {
+                
+                const name = value.src.split('/')  
+                const detail = details(value.src_size, value.dest_size, value.percent);
+
+                ReactDOM.render(
+                    <>
+                        <div className="details">
+                            <article className="detail-compress">
+                                <h5>Before Compress</h5>
+                                <p className="before-compress">{ detail.beforeCompress }</p>
+                            </article>
+                            <article className="detail-compress">
+                                <div id="percent"></div>
+                            </article>
+                            <article className="detail-compress">
+                                <h5>After Compress</h5>
+                                <p className="after-compress">{ detail.afterCompress }</p>
+                            </article>
+                        </div>
+                        <a href={link} className="download" onClick={function (e) {
+                            e.preventDefault();
+                            downloadCompressedImage(link, 'compressed_' + name[name.length - 1]);
+                        }} >Download Image
+                        </a>
+                        <a href="/"
+                            className="again"
+                            onClick = {function (e) {
+                            e.preventDefault();
+                            ReactDOM.render(<UseUrl /> , document.getElementsByTagName('main')[0]);
+                        }} >Do it again?
+                        </a>
+                    </>, document.querySelector('main')
+                )
+                
+                const chart = new ApexCharts(document.querySelector("#percent"), detail.percent);
+                chart.render(); //render chart untuk persenan hasil kompress
+
+                clickButton.finish(e); // aktifkan lagi button
+
+                return alert(`Success`, value);
+            } else {
+                handleErrorFromCompressImage(value.error);
+                clickButton.finish(e); // aktifkan lagi button
+            }   
+        },
+        function (error) {
+            clickButton.finish(e);
+            return alert(`Failed : ${error}. Try again later.`);
+        }
+    );
+}
 
 function UseUrl() {
-    document.querySelectorAll('.choice-item button')[1].classList.value = 'active'; // ketika diklik tambahkan class active
-    document.querySelectorAll('.choice-item button')[0].classList.value = ''; // ketika diklik kosongkan class di upload file
+    const button = document.querySelectorAll('.choice-item button')
+    if (button.length !== 0) {
+        button[1].classList.value = 'active'; // ketika diklik tambahkan class active
+        button[0].classList.value = ''; // ketika diklik kosongkan class di upload file        
+    }
 
-    return ReactDOM.render(
-        <p>ini url</p>,
-        document.getElementsByTagName('main')[0]
+    return (
+        <form onSubmit={compress}>
+            <div className="input-url">
+                <input 
+                    type="url" 
+                    id="url"
+                    placeholder="Enter Url Here"
+                    required
+                />
+                <button type="submit" >Compress Now</button>
+            </div>
+            <span style={{display : 'none'}} className="error">*Please enter an URL</span>
+        </form>
     )
 }
 
